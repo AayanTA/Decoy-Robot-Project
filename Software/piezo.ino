3775bffa45cedcb36c6ee8cce2f1b35360f549c0
AayanTA <158559828+AayanTA@users.noreply.github.com>
2024-09-06 10:09:32 +1000

    Update piezo.ino

--- Software/piezo.ino
+++ Software/piezo.ino
@@ -1,1 +1,17 @@
-# buzz
+const int buzzerPin = 9; //buzzer to arduino pin 9
+
+
+void setup(){
+   
+  pinMode(buzzerPin, OUTPUT); // Set buzzer - pin 9 as an output
+
+}
+
+void   loop(){
+ 
+  tone(buzzerPin, 1000); // Send 1KHz sound signal...
+  delay(1000);         // ...for 1 sec
+  noTone(buzzerPin);     // Stop sound...
+  delay(1000);         // ...for 1sec
+  
+}
